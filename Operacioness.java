42117b5e4ad2b950ac7d660c94506443e5a152b1
Charmandiox9 <daninsoi924@gmail.com>
2023-01-29 18:10:21 -0300

    Se agregaron mas archivos

---
+++ Operacioness.java
@@ -0,0 +1,25 @@
+public class Operacioness{
+    public static void main(String args[]) {
+
+        int operacion =  4;
+        int num_1 = 8;
+        int num_2 = 4;
+        int resultado = 0;
+
+        if(operacion == 1) {
+            resultado = num_1 + num_2;
+            System.out.println("El resultado de la suma es: "+ resultado);
+        } else if(operacion == 2) {
+            resultado = num_1 - num_2;
+            System.out.println("El resultado de la resta es: "+ resultado);
+        } else if(operacion == 3) {
+            resultado = num_1 * num_2;
+            System.out.println("El resultado de la multiplicación es: "+ resultado);
+        } else if(operacion == 4) {
+            resultado = num_1 / num_2;
+            System.out.println("El resultado de la división es: "+ resultado);
+        } else {
+            System.out.println("La opción no es válida");
+        }
+    }
+}
